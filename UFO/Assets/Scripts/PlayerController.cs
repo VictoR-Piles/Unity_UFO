4b070e95f55d4ebe36d478083ad2b33c19cf0333
VictoR-Piles <victorpilesdelgado@gmail.com>
2020-05-30 22:48:15 +0200

    Movimiento del jugador y colisiones

---
+++ UFO/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float velocity = 5f;														// Permite establecer un multiplicador de velocidad desde Unity
+
+	private Rigidbody2D rb2d;
+	
+	// ==================== START ====================
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D>();											// Permite acceder al Rigidbody2D
+	}
+
+	// ==================== UPDATE ====================
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");							// Input para movimientos horizontales
+		float moveVertical = Input.GetAxis("Vertical");								// Input para movimientos verticales
+		
+		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+		rb2d.AddForce(movement * velocity);											// Añade las fuerzas de movimiento al Player
+	}
+}
