f8c6346ed5287bf7d22c1104a5246786f86fb028
VictoR-Piles <victorpilesdelgado@gmail.com>
2020-05-31 12:25:46 +0200

    UI y Pickups terminados

--- UFO/Assets/Scripts/PlayerController.cs
+++ UFO/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 /**
  * Se encarga de mover al Player a través
  * de la informacion obtenida a través del
@@ -9,13 +11,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 	public float velocity = 5f;														// Permite establecer un multiplicador de velocidad desde Unity
-
+	public Text countText;
+	public Text winText;
+	
 	private Rigidbody2D rb2d;
+	private int count;
 	
 	// ==================== START ====================
 	void Start()
 	{
 		rb2d = GetComponent<Rigidbody2D>();											// Permite acceder al Rigidbody2D
+		count = 0;
+		SetCountText();
+		winText.text = "";
 	}
 
 	// ==================== UPDATE ====================
@@ -26,5 +34,25 @@ public class PlayerController : MonoBehaviour
 		
 		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 		rb2d.AddForce(movement * velocity);											// Añade las fuerzas de movimiento al Player
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);										// Destruye los Pickups cuando el Player colisiona con ellos
+			count++;																// Contador de Pickups recogidos
+			SetCountText();															 // Actualiza el texto del contador
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = ($"Count: {count.ToString()}");
+
+		if (count >= 8)
+		{
+			winText.text = "YOU WIN!!!";
+		}
 	}
 }
